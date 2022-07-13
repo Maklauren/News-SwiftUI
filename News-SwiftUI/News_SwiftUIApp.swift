ca0a461b9160084de699323587a654fac21134a0
Pavel Chernook <Maklauren97@gmail.com>
2022-07-13 11:26:51 +0300

    Initial commit

---
+++ News-SwiftUI/News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_SwiftUIApp.swift
+//  News-SwiftUI
+//
+//  Created by Павел Черноок on 13.07.22.
+//
+
+import SwiftUI
+
+@main
+struct News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
